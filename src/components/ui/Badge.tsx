dd3cde40d3bfbe408c84aa17ba44d07d05bc79da
Riyaaa10 <115863480+Riyaaa10@users.noreply.github.com>
2025-02-01 17:12:49 +0530

    Delete src directory

--- src/components/ui/Badge.tsx
+++
@@ -1,8 +0,0 @@
-export const Badge = ({ children }) => {
-    return (
-      <span className="bg-gray-200 text-gray-800 px-2 py-1 rounded-full text-sm">
-        {children}
-      </span>
-    );
-  };
-  
